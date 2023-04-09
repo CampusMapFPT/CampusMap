e8321bd6a7c9e3a866b55edd6064108d8d107b8e
datuwu <datlt.mdc@gmail.com>
2023-04-09 19:14:10 +0700

    fix cors header

--- components/direction/components/feedbackModal/feedbackModal.tsx
+++ components/direction/components/feedbackModal/feedbackModal.tsx
@@ -34,13 +34,7 @@ const FeedbackModal = () => {
             }
 
             const headers = {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                "Access-Control-Allow-Credentials": "true",
-                "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-                "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
-                'Accept': 'application/json, text/plain, */*'
-
+                'access-control-allow-origin': '*',
             };
             await axios.post(
                 API_FEEDBACK,
