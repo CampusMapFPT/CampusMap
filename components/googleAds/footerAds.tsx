1e248ae4980360b997fc3e3c7d38d9c9b9609c90
datuwu <datlt.mdc@gmail.com>
2023-04-23 15:51:24 +0700

    Fix ads

--- components/googleAds/footerAds.tsx
+++ components/googleAds/footerAds.tsx
@@ -3,11 +3,9 @@ import footerAdImage from '../../public/assets/images/ads/footerAd.jpg'
 
 const FooterAds = () => {
     return (
-        <Flex className="ads" >
-            <Image src={'https://firebasestorage.googleapis.com/v0/b/campus-map-f1a9f.appspot.com/o/ads%2FfooterAd.jpg?alt=media&token=87b95650-d62a-45eb-93f7-b07b32124196'}
-                alt={"ads img"}
-                width='100%'></Image>
-        </Flex>
+        <Image src={'https://firebasestorage.googleapis.com/v0/b/campus-map-f1a9f.appspot.com/o/ads%2FfooterAd.jpg?alt=media&token=87b95650-d62a-45eb-93f7-b07b32124196'}
+            alt={"ads img"}
+            width='100%'></Image>
     )
 }
 
